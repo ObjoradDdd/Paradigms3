617af6d8d19c1a1fbb47a0bee131b6bf70de2c31
ObjoraDdd <trunova397@gmail.com>
2025-04-18 15:06:42 +0700

    \\\

--- main.cpp
+++ main.cpp
@@ -8,27 +8,16 @@ int main() {
     int groupCount, devicesPerGroup, queueSize;
     
 
-    cout << "Enter number of groups (n > 2): ";
+
     cin >> groupCount;
     
-    if (groupCount <= 2) {
-        cout << "Number of groups must be greater than 2!\n";
 
-    }
-    
-    cout << "Enter devices per group (m > 2): ";
     cin >> devicesPerGroup;
     
-    if (devicesPerGroup <= 2) {
-        cout << "Number of devices must be greater than 2!\n";
-    }
-    
-    cout << "Enter queue capacity: ";
+
+
     cin >> queueSize;
     
-    if (queueSize <= 0) {
-        cout << "Queue capacity must be positive!\n";
-    }
 
     auto* system = new queueing(groupCount, devicesPerGroup, queueSize);
     system->start();
